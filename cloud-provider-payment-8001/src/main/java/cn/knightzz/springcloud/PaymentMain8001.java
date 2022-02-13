4b4b56961e6dd523d06d1a4ce44c147118c22709
王天赐 <15565946702@163.com>
2022-02-13 21:56:08 +0800

    :art: 创建cloud-provider-payment-8001 项目

---
+++ cloud-provider-payment-8001/src/main/java/cn/knightzz/springcloud/PaymentMain8001.java
@@ -0,0 +1,20 @@
+package cn.knightzz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王天赐
+ * @title: PaymentMain8001
+ * @projectName project-alibaba
+ * @description:
+ * @website http://knightzz.cn/
+ * @github https://github.com/knightzz1998
+ * @date 2022/2/13 20:34
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
